56b6ceef21df1dd3050c10156cdd01c1beffed3b
gpulch <42658765+gpulch@users.noreply.github.com>
2022-10-05 21:19:08 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//bouton check : click = verifier si doublons dans ligne / col / groupe 3x3
+const check = () => {
+const listeInput = document.querySelectorAll('input')
+console.log
+}
+
+const buttonCheck = document.getElementById('check')
+buttonCheck.addEventListener('click', check)
+
+//bouton reset : click = reinitialiser toutes les cellules et placer un nombre aleatoire
+
